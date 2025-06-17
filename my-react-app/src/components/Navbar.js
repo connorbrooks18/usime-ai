2c76f926ba8fdc51f01b1736f7aab36d082aba5c
MarksWorkspace <markbrooks1207@gmail.com>
2025-06-16 20:11:02 -0500

    Started Creating page for uploading documents to ai

--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -11,10 +11,9 @@ function Navbar() {
         <h1>USIME.AI</h1>
       </div>
       <div className="main-nav">
-        <ul>
-          <li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
+        <ul>          <li><Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link></li>
           <li><Link to="/about" className={location.pathname === '/about' ? 'active' : ''}>About</Link></li>
-          <li><Link to="/services" className={location.pathname === '/services' ? 'active' : ''}>Services</Link></li>
+          <li><Link to="/document-upload" className={location.pathname === '/document-upload' ? 'active' : ''}>Document Upload</Link></li>
           <li><Link to="/create-ime" className={location.pathname === '/create-ime' ? 'active' : ''}>Submit IME</Link></li>
         </ul>
       </div>
