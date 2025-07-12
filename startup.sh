50748bd815af5ee6ce7f12493805ad46e9e5bae5
connorbrooks18 <connorbrooks18@icloud.com>
2025-07-12 16:50:49 -0500

    Add debugging to startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /home/site/wwwroot
+echo "Installing dependencies..."
 pip install -r requirements.txt
+echo "Dependencies installed successfully"
 cd Backend
+echo "Starting Flask app..."
 python server.py 
