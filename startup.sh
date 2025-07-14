dc3357c0785d6445e6fc5fb575db1ea3baff393a
connorbrooks18 <connorbrooks18@icloud.com>
2025-07-13 23:06:25 -0500

    add poppler to startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 
 # Install Tesseract OCR quickly
 #apt-get update && apt-get install -y tesseract-ocr
+apt-get update && apt-get install -y poppler-utils
+
 
 # Start the Flask app with gunicorn
 gunicorn --bind 0.0.0.0:8000 app:app 
